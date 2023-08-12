39c8226e82fcbbfc6369052cec7d60c7f6adb131
Emtiaj Hasan <271emtiaj@gmail.com>
2023-08-12 08:14:24 +0600

    --if-exists added to avoid getting error due to function public.getRandomString(integer)

--- scripts/db-restore.bash
+++ scripts/db-restore.bash
@@ -1,4 +1,4 @@
 docker cp db-dump query-comparison-postgres:/dump/;
 database=query-comparison;
-docker exec query-comparison-postgres pg_restore --username=postgres --dbname=$database --clean --verbose dump/$database;
+docker exec query-comparison-postgres pg_restore --username=postgres --dbname=$database --clean --verbose --if-exists dump/$database;
 docker exec query-comparison-postgres sh -c 'rm -rf dump';
